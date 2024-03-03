a2aa6f57c34aa1a1441df083864720a420c7c627
Elijah <ehh00004@mix.wvu.edu>
2024-03-02 20:39:48 -0500

    Switch to VSCode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import './NavBar';
+import './Card';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
+          <NavBar />
           <h1>CS 230L</h1>
           <h2>Section - 002</h2>
           <p>WVU ID: 800384692</p>
           <p>Hi I am Elijah</p>
+          <p>Test to see if it is updating</p>
+          <Card />
     </div>
   );
 }
