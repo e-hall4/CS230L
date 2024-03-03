8e3471703fcb74d20fce252ff911b7ab4b682bd8
Elijah <ehh00004@mix.wvu.edu>
2024-03-03 17:48:53 -0500

    Week 8 Complete

--- src/Card.js
+++ src/Card.js
@@ -5,38 +5,32 @@ function Card() {
     return (
         <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
         <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm">
-            <div class="card-header py-3">
+          <div class="card mb-4 rounded-3 shadow-sm border-dark">
+            <div class="card-header py-3 text-bg-dark border-light">
               <h4 class="my-0 fw-normal">One Ring</h4>
             </div>
-            <div class="card-body">
-              <ul class="list-unstyled mt-3 mb-4">
-                <li>To find them.</li>
-              </ul>
+            <div class="card-body text-bg-dark">
+              <p class="mt-3 mb-4 text-white">To rule them all.</p>
             </div>
           </div>
         </div>
         <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm">
-            <div class="card-header py-3">
-              <h4 class="my-0 fw-normal">ONE Ring</h4>
+          <div class="card mb-4 rounded-3 shadow-sm border-dark">
+            <div class="card-header py-3 text-bg-dark border-light">
+              <h4 class="my-0 fw-normal text-warning">One Ring</h4>
             </div>
-            <div class="card-body">
-              <ul class="list-unstyled mt-3 mb-4">
-                <li>To bind them.</li>
-              </ul>
+            <div class="card-body text-bg-dark">
+              <p class="mt-3 mb-4 text-warning">To find them.</p>
             </div>
           </div>
         </div>
         <div class="col">
-          <div class="card mb-4 rounded-3 shadow-sm border-primary">
-            <div class="card-header py-3 text-bg-primary border-primary">
-              <h4 class="my-0 fw-normal">ONE RING</h4>
+          <div class="card mb-4 rounded-3 shadow-sm border-dark">
+            <div class="card-header py-3 text-bg-dark border-light">
+              <h4 class="my-0 fw-normal text-danger">One Ring</h4>
             </div>
-            <div class="card-body">
-              <ul class="list-unstyled mt-3 mb-4">
-                <li>to bring them all and in the darkness bind them.</li>
-              </ul>
+            <div class="card-body text-bg-dark">
+              <p class="mt-3 mb-4 text-danger">To bring them all and in the darkness bind them.</p>
             </div>
           </div>
         </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -7,14 +7,14 @@ function NavBar() {
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#ring" class="nav-link px-3 text-gold">The One Ring</a></li>
-                        <li><a href="#home" class="nav-link px-2 text-white">Mordor</a></li>
+                        <li><a href="#ring" class="nav-link px-3 text-warning">The One Ring</a></li>
+                        <li><a href="#home" class="nav-link px-2 text-white">Home</a></li>
                         <li><a href="#about" class="nav-link px-2 text-white">About</a></li>
-                        <li><a href="#contact" class="nav-link px-2 text-white">Contact Sauron</a></li>
+                        <li><a href="#contactSauron" class="nav-link px-2 text-white">Contact Sauron</a></li>
                     </ul>
 
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                        <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
+                        <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search"/>
                     </form>
                 </div>
             </div>
