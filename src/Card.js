13d2a93790ce40eab0d8decf3f593c970ece6e2d
Elijah <ehh00004@mix.wvu.edu>
2024-03-03 16:57:53 -0500

    Card prototype

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,44 @@ import React from 'react';
 
 function Card() {
     return (
-        <div>
-            <p>Cards will go here</p>
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">One Ring</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-unstyled mt-3 mb-4">
+                <li>To find them.</li>
+              </ul>
+            </div>
+          </div>
         </div>
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">ONE Ring</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-unstyled mt-3 mb-4">
+                <li>To bind them.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-primary">
+            <div class="card-header py-3 text-bg-primary border-primary">
+              <h4 class="my-0 fw-normal">ONE RING</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-unstyled mt-3 mb-4">
+                <li>to bring them all and in the darkness bind them.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -7,10 +7,10 @@ function NavBar() {
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" class="nav-link px-1 text-secondary">Navbar</a></li>
-                        <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
-                        <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-                        <li><a href="#" class="nav-link px-2 text-white">Contact</a></li>
+                        <li><a href="#ring" class="nav-link px-3 text-gold">The One Ring</a></li>
+                        <li><a href="#home" class="nav-link px-2 text-white">Mordor</a></li>
+                        <li><a href="#about" class="nav-link px-2 text-white">About</a></li>
+                        <li><a href="#contact" class="nav-link px-2 text-white">Contact Sauron</a></li>
                     </ul>
 
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
